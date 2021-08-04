5e36acb11b6b749c7bdaa59d1e73ab5506914e51
mankaistep <hhoang21vn@gmail.com>
2021-08-04 23:36:24 +0700

    Cancel jumping while showing titles

--- src/main/java/mk/plugin/messages/listener/EventListener.java
+++ src/main/java/mk/plugin/messages/listener/EventListener.java
@@ -1,5 +1,6 @@
 package mk.plugin.messages.listener;
 
+import com.google.common.collect.Sets;
 import mk.plugin.messages.main.MainMessages;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -8,12 +9,31 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Set;
+
 public class EventListener implements Listener {
-	
+
+	public Set<String> freezed;
+
+	private final long WAIT = 55;
+
+	public EventListener() {
+		this.freezed = Sets.newHashSet();
+	}
+
+	@EventHandler
+	public void onJump(PlayerToggleFlightEvent e) {
+		var p = e.getPlayer();
+		if (freezed.contains(p.getName())) {
+			e.setCancelled(true);
+		}
+	}
+
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		Player player = e.getPlayer();
@@ -34,12 +54,17 @@ public class EventListener implements Listener {
 					if (l.distance(player.getLocation()) >= plugin.getWelcomeRadius()) {
 						this.cancel();
 
-						int time = plugin.getTitles().size() * 55;
+						freezed.add(player.getName());
+						int time = Long.valueOf(plugin.getTitles().size() * WAIT).intValue();
+
+						Bukkit.getScheduler().runTaskLater(plugin, () -> {
+							freezed.remove(player.getName());
+						}, time);
+
 						Bukkit.getScheduler().runTask(plugin, () -> {
 							player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, time, 10, false, false, false));
 							player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, time, 10, false, false, false));
 						});
-
 
 						int c = 0;
 						for (String s : plugin.getTitles()) {
@@ -48,7 +73,7 @@ public class EventListener implements Listener {
 								var st = s.split(";")[1];
 								player.sendTitle(t.replace("%player%", player.getName()), st.replace("%player%", player.getName()), 0, 200, 0);
 								player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1, 1);
-							}, 55L * c);
+							}, WAIT * c);
 							c++;
 						}
 					}
